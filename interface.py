af7a79c9cc71490841ebebbc3f65d438b5f2c39b
WilliamXXu <wl_xxz@protonmail.com>
2021-10-07 00:17:49 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -7,16 +7,17 @@ cur_dict={'US':'USD','UK':'GBP','HK':'HKD','CN':'CNY','EU':'EUR','CH':'CHF'}
 a=Engine()
 
 while True:
-	i=input('---------------------------------\nHow can I serve you, Commander?\n 0. view portofolio (updated price) \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4.retrieve temporary data\n---------------------------------\n')
+	i=input('---------------------------------\nHow can I serve you, Commander?\n u. update \n 0. view portofolio (updated price) \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4.retrieve temporary data\n---------------------------------\n')
 
-	if i=='0':
+	if i =='u':
 		a.update()
+	if i=='0':
 		repr(a)
-		print('')
-
+'''
 		from datetime import datetime
 		now = datetime.now()
 		input('updated at '+now.strftime("%d/%m/%Y %H:%M:%S")+'\nPress Enter to continue\n')
+		'''
 
 	if i=='1':
 		division=input('Which market? (CN,US,UK,HK,EU,CH....)\n')
