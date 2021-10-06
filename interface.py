1399ced6982a75da975777234c24d6ac434e67ac
WilliamXXu <wl_xxz@protonmail.com>
2021-10-06 23:23:44 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -30,7 +30,9 @@ while True:
 		symbol=input('Instrument symbol? Google Finance format (eg. 000001:SHE)\n')
 		quantity=input('How many shares you buy/sell?\n')
 		cost=input('For how much you buy/sell it?\n')
-		fee=input('Estimate the commission please\n')
+		fee=input('Estimate the commission please. Press Enter if zero\n')
+		if not len(currency):
+			fee='0'
 
 	if i=='1':
 		a.trade(symbol,quantity,cost,fee,currency,division,alis)
