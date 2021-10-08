aa819430f6fe507f9a0087cb9a24e7cd2ebb11da
WilliamXXu <wl_xxz@protonmail.com>
2021-10-09 00:39:12 +0100

    big change

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 #tetst message
 class Instrument():
 
-	def __init__(self,sym,quan,cost,currency,division,alis,price):
+	def __init__(self,sym,quan,cost,currency,division,alis,sym1,price):
 
 		self.sym=sym
 		self.currency=currency
@@ -10,10 +10,13 @@ class Instrument():
 		self.cost=cost
 		self.price=price
 		self.alis=alis
+		self.sym1=sym1
+		self.properties=dict()
 		
 class Engine():
 	def __init__(self):
-		import pickle	
+		import pickle
+		self.properties={'Beta','PE','ROE','Yield'}	
 		try:
 			with open('data.pickle','rb') as g:
 				temp=pickle.load(g)
@@ -44,10 +47,13 @@ class Engine():
 
 	def yfi(self,symbol):
 		import yfinance as yf
-		res=yf.Ticker(symbol)
-		p=res.info['currentPrice']
-		print(symbol+'\n')
-		return p
+		mp={'Beta':'beta','PE':'trailingPE','ROE':'returnOnEquity','Yield':'trailingAnnualDividendYield'}
+	
+		d=dict()
+		res=yf.Ticker(symbol).info
+		for x in mp:
+				d[x]=res[mp[x]]
+		return d
 
 	def google(self,symbol):
 		from bs4 import BeautifulSoup
@@ -66,16 +72,23 @@ class Engine():
 	def update(self):
 		li=list(self.stocks.keys())
 		for x in li:
-			self.stocks[x].price=self.yfi(self.stocks[x].sym)
+			self.stocks[x].price=self.google(self.stocks[x].sym)
+
+	def propertyUpdate(self):
+		li=list(self.stocks.keys())
+		for x in li:
+			self.stocks[x].properties=self.yfi(self.stocks[x].sym)
 
 	def __repr__(self):
 		import pandas as pd
-		
+
 		print('\n\n')
-		li=[]
+		
 
 		for x in self.stocks:
 			t=vars(self.stocks[x])
+			for y in self.properties:
+				t[y]=x.properties[y]
 			li.append(pd.DataFrame(t,index=['?']))
 
 		if len(li)>0:
@@ -114,8 +127,10 @@ class Engine():
 				args.append('?')
 					
 			try:
-				self.stocks[sym]=Instrument(sym,quan,cost,args[0],args[1],args[2],self.yfi(sym))
+				self.stocks[sym]=Instrument(sym,quan,cost,args[0],args[1],args[2],args[3],self.google(sym))
+				self.stocks[sym].properties=self.yfi(sym)
 				self.cash(self.stocks[sym].currency,(-cost)*quan)
+
 			except IndexError:
 				raise IndexError('first time position or existing position? also check ur symbol')
 
@@ -128,7 +143,7 @@ class Engine():
 			else:
 				obj.cost=(obj.quan*obj.cost+quan*cost)/new_quan
 				obj.quan=new_quan
-				self.stocks[sym].price=self.yfi(sym)
+				self.stocks[sym].price=self.google(sym)
 		self.save_temp()
 
 
--- interface.py
+++ interface.py
@@ -2,15 +2,30 @@ from engine import Engine
 #换手率 波动率 做t rank by   division  level  我草泥马
 
 cur_dict={'US':'USD','UK':'GBP','HK':'HKD','CN':'CNY','EU':'EUR','CH':'CHF'}
-
+trans={':LON':'.L',':HKG':'.HK',':BME':'.MC',':NYSE':'',':OTCMKTS':'',':ETR':'.DE'}
 
 a=Engine()
 
 while True:
-	i=input('---------------------------------\nHow can I serve you, Commander?\n u. update \n 0. view portofolio (updated price) \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4.retrieve temporary data\n---------------------------------\n')
+	i=input('---------------------------------\nHow can I serve you, Commander?\n u. price update \n uu. full update \n 0. view portofolio (updated price) \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4.retrieve temporary data\n 5. manage additional indicators\n---------------------------------\n')
 
 	if i =='u':
 		a.update()
+	if i =='uu':
+		a.update()
+		a.propertyUpdate()
+	if i =='5':
+		print('Current indicators: '+a.properties+' \n')
+		print('You can choose from: Beta, PE, ROE, Yield\n')
+		while 1:
+			ind=input('The indicator that you want to add or remove? Press Enter to exit.\n')
+			if not len(ind):
+				break
+			elif ind in a.properties:
+				a.properties.remove(ind)
+			else:
+				a.properties.add(ind)
+
 	if i=='0':
 		repr(a)
 		input('\nPress Enter to continue\n')
@@ -20,12 +35,16 @@ while True:
 		if not len(currency):
 			currency=cur_dict[division]
 		alis=input('Alis for this instrument?Press Enter if none\n')
-
-
+		symbol=input('Instrument symbol? Google Finance format.\n')
+		symbol1=input('Instrument symbol? Yahoo Finance format. Press Enter if can be inferred\n')
+		if not len(symbol1):
+			for y in trans.keys():
+				if y in symbol1:
+					symbol1=symbol1.replace(y,trans[y])
+	if i=='2':
+		symbol=input('Instrument symbol? Google Finance format.\n')
 
 	if i=='1' or i=='2':
-		symbol=input('Instrument symbol? Yahoo Finance format (eg. 0688.HK)\n')
-		#symbol1=input('Instrument symbol? Google Finance format (eg. 000001:SHE)\n')
 		quantity=input('How many shares you buy/sell?\n')
 		cost=input('For how much you buy/sell it?\n')
 		fee=input('Estimate the commission please. Press Enter if zero\n')
@@ -33,7 +52,7 @@ while True:
 			fee='0'
 
 	if i=='1':
-		a.trade(symbol,quantity,cost,fee,currency,division,alis)
+		a.trade(symbol,quantity,cost,fee,currency,division,alis,symbol1)
 	if i=='2':
 		a.trade(symbol,quantity,cost,fee)
 
