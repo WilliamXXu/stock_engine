98343853fd9ae0717d5ab03f8b536f52f47b7c77
WilliamXXu <wl_xxz@protonmail.com>
2021-10-09 01:02:13 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -42,6 +42,7 @@ while 1:
 			for y in trans.keys():
 				if y in symbol1:
 					symbol1=symbol1.replace(y,trans[y])
+					print(symbol1)
 	if i=='2':
 		symbol=input('Instrument symbol? Google Finance format.\n')
 
@@ -53,6 +54,7 @@ while 1:
 			fee='0'
 
 	if i=='1':
+		print(symbol1)
 		a.trade(symbol,quantity,cost,fee,currency,division,alis,symbol1)
 	if i=='2':
 		a.trade(symbol,quantity,cost,fee)
