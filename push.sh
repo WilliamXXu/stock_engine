2ef8a8c1dce2a4d603847bc89f85ec08252b364c
WilliamXXu <wl_xxz@protonmail.com>
2021-10-30 00:32:02 +0100

    d

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 cd stock_engine
 git add .
 git commit -m null
-git push https://WilliamXXu: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@github.com/WilliamXXu/stock_engine.git main
+git push https://WilliamXXu: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V@github.com/WilliamXXu/stock_engine.git main
