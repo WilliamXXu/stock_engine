cad7e2b164358df7e0ddf05e8a23b7fdc8f824fe
WilliamXXu <wl_xxz@protonmail.com>
2021-10-30 01:31:34 +0100

    null

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ trans={':LON':'.L',':HKG':'.HK',':BME':'.MC',':NYSE':'',':OTCMKTS':'',':NASDAQ':
 a=Engine()
 
 while 1:
-	i=input('---------------------------------\nHow can I serve you, Commander?\n u. price update \n uu. full update \n 0. view portofolio \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4. retrieve temporary data\n 5. manage additional indicators\n 6.advanced analytics\n---------------------------------\n')
+	i=input('---------------------------------\nHow can I serve you, Commander?\n u. price update \n uu. full update \n 0. view portofolio \n 1. open a new position \n 2. deal with an existing position \n 3. Save changes \n 4. retrieve temporary data\n 5. manage additional indicators\n 6. advanced analytics\n---------------------------------\n')
 	if i =='u':
 		a.update()
 	if i =='uu':
@@ -81,7 +81,7 @@ while 1:
 	if i == '6':
 		from analytics import Analytics
 		b=Analytics(a.stocks,a.money,a.prepareDf())
-		print('\ntotal asset is: ')
+		print('\ntotal asset (HKD) is: ')
 		print(b.asset)
 		print('\naverage yield (TTM) is: ')
 		print(b.average_div())
